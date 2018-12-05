2478e1aa4b6c90a677e02e58c5bb0e8b526b599a
Simon <ggeyangbo@163.com>
2018-12-05 23:45:12 +0800

    添加项目文件。

---
+++ C_Learning/hello.c
@@ -0,0 +1,10 @@
+/**
+This is my first C programe ,I'm very glad to restart to enjoy my way!
+Simon 20181205
+*/
+#include<stdio.h>
+int main() {
+	printf("hello world \n");
+	system("pause");
+	return 0;
+}
